697d35d996ed1de051ede58a4b43a7f98611ce78
LykkeBergstein <1086113@ucn.dk>
2022-06-05 12:10:23 +0200

    Desktop mostly done

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,16 +2,19 @@ articlesJS = `  [
      { 
         "title": "Der er sat strøm til Danmarks nye gamingevent ",  
         "date": "24. april 2022", 
-        "picture": " ", 
-        "photo-credit": "MCH/Theis Poulsen", 
+        "picture": " assets/img/article1.png",  
+        "pictureDescription": "Et hav af forskellige esportsgrene, spil og gamingaktiviteter var repræsenteret på Gamebox Festival, der havde debut 22.-24. april i Herning. ", 
+        "photocredit": "MCH/Theis Poulsen", 
+
         "description": "Denne weekend blev kablerne for første gang koblet til Gamebox Festival, Danmarks nye, store esport- og gamingevent, som samlede tusindvis af gamere, streamere, prospillere, nybegyndere og nysgerrige om alskens gamingaktiviteter og professionelle turneringer i MCH Messecenter Herning. ", 
-        "article":  " " 
+        "article": " Der blev gamet til den helt store guldmedalje, da Gamebox Festival – Danmarks nye esport- og gamingevent – denne weekend debuterede i MCH Messecenter Herning. Her kunne besøgende overvære digitale danmarksmesterskaber, tempofyldte turneringer og storslåede showkampe, ligesom de selv kunne prøve kræfter med alt fra arkademaskiner til konsol- og computerspil. 8.642 besøgte festivalen, mens flere end 300.000 fulgte med online via streams.  <br> <br> Hos MCH A/S glæder administrerende direktør Georg Sørensen sig over den positive modtagelse, som nyskabelsen, Gamebox Festival, har fået. <br> <br> - At lancere et nyt event er altid noget særligt. Og at gøre det indenfor en ny branche med en helt ny målgruppe er noget helt særligt. Vi har udviklet Gamebox Festival fra bunden og - som det også altid gør sig gældende med vores messer – i tæt samarbejde med både organisationer og de førende aktører i branchen, siger Georg Sørensen og understreger: <br> <br> - Derfor er de mange positive tilkendegivelser helt afgørende, og der skal lyde en stor tak til vores mange samarbejdspartnere, som har været med til at skabe en unik oplevelse for de tusindvis af gaminginteresserede besøgende. </p> <br> <br> <p class='boldText'> Bredde, elite, nonstop gaming og giveaway </p>  <p > Kendte prospillere og store streamere stod flere steder klar til at møde deres fans og tage en dyst på de virtuelle slagmarker. Det gjorde sig blandt andet gældende i Astralis’ store fanzone, som weekenden igennem var et tilløbsstykke for såvel rutinerede gamere som nybegyndere. Og netop det faktum har været altafgørende for festivalens succes, mener marketingchef i Astralis Benjamin Boraghi. <br> <br> - Gamebox Festival kan det hele; hallerne har været fyldt op med aktiviteter, konkurrencer og turneringer for alle aldre og alle niveauer – det er ikke et nicheevent, men noget for både bredden og eliten, og det er altafgørende, for det ene kunne ikke eksistere uden det andet. Gamebox Festival har været en kæmpemæssig fejring af al form for gaming og esport i Danmark, og derfor har Astralis helt naturligt taget del i festen, fortæller Benjamin Boraghi. <br> <br> Også hos Elgiganten ser man det som festivalens store styrke, at den favner bredt. Elektronikkæden havde til lejligheden bygget en 800 kvadratmeter stor popup-butik og inviterede desuden til et populært giveaway-show med produkter for 85.000 kr. <br> <br> - Gamebox Festival har budt på det højeste niveau, der nogensinde er set inden for denne slags events i Danmark. Festivalen samler alt inden for gaming og esport – erhverv, små foreninger, kommercielle partnere, prospillere og amatører – og derfor kan der heller ikke være et event som Gamebox Festival, uden Elgiganten er med. Gamebox Festival er kommet for at blive, og det er Elgigantens involvering og engagement i eventet også. Det har været en fantastisk weekend, fastslår assisterende salgschef Casper Mortensen. <br> <br> Bredden var blandt andet repræsenteret i den store LAN-hal, hvor hundredvis af unge fredag til søndag var samlet til nonstop gaming med både gear og pengepræmier på højkant. Også her herskede der ingen tvivl om eventets høje standard: <br> <br> - Vi samarbejder med Gamebox Festival for at være med til at skabe et nyt, stærkt årligt event, som vores medlemmer kan deltage i og se frem til, og det har vi i dén grad fået. Det her er nørdernes paradis. Faciliteterne har været alt, hvad vi kunne drømme om – det er de bedste på dansk jord til et LAN-event som dette, fortæller Mark Kristensen, direktør i Sørby Esport. </p> <br> <br> <p class='boldText'> Guldregn over grønsværen og de virtuelle vejbaner </p> <p> Gamebox Festival blændede weekenden igennem op for flere prestigefyldte og professionelle turneringer. Således blev blandt andet danmarksmesterskabet i digital motorsport, Porsche Esports Carrera Cup, afgjort på hovedscenen lørdag aften, hvor racerkører Mikkel Gade vandt førstepræmien på 50.000 kr. <br> <br> Verdens største 1v1-turnering i League of Legends, Red Bull Solo Q, udspillede sig også på hovedscenen, hvor Carl 'Curl' Gyldenkærne sikrede sig en billet til den internationale finale. De første danske mestre i DBU eFodbold blev også fundet på festivalen, og her blev det Starup UIF og Nordens Paris FC, der kunne løfte pokalerne i henholdsvis U13-16 og U17+. 14 eSuperligaklubber var i kamp i FIFA-turneringen GameBoks Cup, og her løb SønderjyskE Esport med ære, hæder og præmiepenge til et velgørende formål. Cosplayeren Meruna modtog tillige hyldest med en førsteplads for bedste udklædning. <br> <br> Sidst, men ikke mindst bød festivalen på den store CS:GO-turnering Gamebox Invitational, hvor otte hold – amatører som etablerede – weekenden igennem dystede om en plads på podiet. Det blev Astralis Talent, der kunne trække sig sejrrigt ud af søndagens finale. <br> <br> Gamebox Festival blev gennemført 22.-24. april i samarbejde med main partners Jyske Bank, Elgiganten, hummel, Carlsberg Sport og IT Relation samt supporting partners DGI, DIF, Esport Danmark, Astralis, Ungdomsringen, Dansk Firmaidrætsforbund, HLTV og NetParty Herning. <br> <br> Festivalen vender tilbage i 2023. " 
+
     } , 
     { 
         "title": "Gamebox Festival er i fuld gang i Herning ",  
         "date": "23. april 2022", 
         "picture": " ", 
-        "photo-credit": "MCH/Theis Poulsen", 
+        "photocredit": "MCH/Theis Poulsen", 
         "description": "Fredag blev dørene for første gang åbnet til Danmarks nye, store esport- og gamingevent, Gamebox Festival, der over tre dage inviterer til en bred vifte af store turneringer og aktiviteter for såvel prospillere, rutinerede gamere som nybegyndere, deres forældre og familier. Førstedagen bød blandt meget andet på direkte duel mellem FCM-stjerne Evander og Astralis-verdenstjerne ”Teca” samt standup med Mark le Fêvre. ",  
         "article":  " " 
     } , 
@@ -19,7 +22,7 @@ articlesJS = `  [
         "title": "Danmarks største gamingevent klar til start ", 
         "date": "22. april 2022", 
         "picture": " ", 
-        "photo-credit": "MCH/Tony Brøchner",  
+        "photocredit": "MCH/Tony Brøchner",  
         "description": "Prestigefyldte turneringer i blandt andet FIFA, CS:GO og League of Legends udspiller sig på weekendens Gamebox Festival i MCH Messecenter Herning, hvor man også kan møde en række kendte streamere og teste sine evner mod professionelle gamere. Også talks, SimRacing, showkampe, et kæmpe LAN-party og et regulært udstyrsmekka er i vente, når Danmarks nye gamingevent fredag 22. april slår dørene op. ", 
         "article":  " " 
     } , 
@@ -27,7 +30,7 @@ articlesJS = `  [
         "title": "Landets bedste FIFA-spillere dyster på Gamebox Festival ",  
         "date": "13. april 2022", 
         "picture": " ", 
-        "photo-credit": "Carl Johan Dahl, Brøndby IF",   
+        "photocredit": "Carl Johan Dahl, Brøndby IF",   
         "description": "Alle de største stjerner fra eSuperligaen indfinder sig på Gamebox Festival i MCH Messecenter Herning 22.-24. april, hvor de i computerspillet FIFA kæmper om hæder og ære og muligheden for at donere penge til velgørenhed. ",  
         "article":  " " 
     } , 
@@ -35,7 +38,7 @@ articlesJS = `  [
         "title": "Verdens største 1v1-turnering i 'League of Legends' rammer Herning ", 
         "date": "5. april 2022", 
         "picture": " ", 
-        "photo-credit": "Marius Faulhaber/Red Bull Content Pool", 
+        "photocredit": "Marius Faulhaber/Red Bull Content Pool", 
         "description": "Den danske finale ved Red Bull Solo Q, verdens største 1v1-turnering i computerspillet 'League of Legends', udspiller sig på Gamebox Festival i MCH Messecenter Herning. Danmarks største gamingevent, der for første gang løber af stablen 22.-24. april, bliver dermed stedet, hvor landets deltager i den internationale finale skal findes. ", 
         "article":  " " 
     } , 
@@ -43,7 +46,7 @@ articlesJS = `  [
         "title": "Porche, Hummel og IT Relation er nye partnere på Gamebox Festival ", 
         "date": "16. marts 2022", 
         "picture": " ", 
-        "photo-credit": "Gamebox Festival", 
+        "photocredit": "Gamebox Festival", 
         "description": "Porsche, hummel og IT Relation ser alle store perspektiver i esport og gaming og ønsker at være en del af den eksplosivt voksende scene som partnere på Gamebox Festival 22.-24. april 2022. De skriver sig dermed ind i rækken af kendte partnerbrands på Danmarks største esport- og gamingevent, der blandt andre også tæller Jyske Bank og Elgiganten. ", 
         "article":  " " 
     } , 
@@ -51,7 +54,7 @@ articlesJS = `  [
         "title": "Porche esports Carrera Cup Danmark afgøres til Gamebox Festival ", 
         "date": "14. marts 2022", 
         "picture": " ", 
-        "photo-credit": "Gamebox Festival", 
+        "photocredit": "Gamebox Festival", 
         "description": "Danmarksmesteren i det officielle danske motorsportsmesterskab, Porsche Esports Carrera Cup Denmark, kåres i et TV-transmitteret liveevent til Danmarks største gamingfestival, Gamebox Festival, i MCH Messecenter Herning 23. april. ", 
         "article":  " " 
     } , 
@@ -59,7 +62,7 @@ articlesJS = `  [
         "title": "Politisk fokus på esport: Højaktuel debat fortsætter på Gamebox Festival ", 
         "date": "3. marts 2022", 
         "picture": " ", 
-        "photo-credit": "Gamebox Festival", 
+        "photocredit": "Gamebox Festival", 
         "description": "Der skal bygges bro mellem generationer, når det kommer til esport og gaming, som netop nu er på den politiske dagsorden på Christiansborg. De højaktuelle debatter fortsætter til april på Danmarks største esport- og gamingevent, Gamebox Festival, der blandt andet vil uddanne forældre i en for mange ukendt verden. ", 
         "article":  " " 
     } , 
@@ -67,7 +70,7 @@ articlesJS = `  [
         "title": "DM i efodbold: Grand final-brag til Gamebox Festival ", 
         "date": "24. februar 2022", 
         "picture": " ", 
-        "photo-credit": "Gamebox Festival", 
+        "photocredit": "Gamebox Festival", 
         "description": "De første danske mestre i DBU eFodbold skal findes 23. april til Gamebox Festival - Danmarks største gamingevent - hvor man også kan møde eFodbold-landsholdet. Det hele foregår i MCH Messecenter Herning, hvor 16 finalehold dyster om at nå til helt til tops og løfte pokalen. ", 
         "article":  " " 
     } 
